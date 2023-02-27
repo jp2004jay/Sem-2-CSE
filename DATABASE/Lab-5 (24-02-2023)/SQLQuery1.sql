c1b73b268dea9ecaa1cc7f95545aee971ea6707a
Ramani Jay <ramanijay2004@outlook.com>
2023-02-27 18:59:56 +0530

    Add files via upload

---
+++ DATABASE/Lab-5 (24-02-2023)/SQLQuery1.sql
@@ -0,0 +1,53 @@
+--1
+select 5*30
+
+--2
+select abs(-25)
+select abs(25)
+select abs(-50)
+select abs(50)
+
+--3
+select floor(25.2),
+ floor(25.7),
+ floor(-25.2)
+
+--4
+select ceiling(25.2)
+select ceiling(25.7)
+select ceiling(-25.2)
+
+--5
+select 5%2, 
+		5%3
+
+--6
+select power(3,2),
+		power(4,3) 
+
+--7
+select sqrt(25), sqrt(30), sqrt(50)
+
+--8 
+select square(5), square(15), square(25)
+
+--9
+select pi()
+
+--10
+select round(157.732, 2), round(157.732, 0), round(157.732, -2)
+
+--11
+select exp(2), exp(3)
+
+--12
+select log(10), log(5), log(100)
+
+--13
+select sin(3.1415), cos(3.1415), tan(3.1415)
+
+--14
+select sign(-25), sign(0), sign(25)
+
+--15
+select rand()
